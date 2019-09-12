b5db31218221085e128e1762a9b04c1942ca5257
Richard J Purcell <42460972+richpurcell@users.noreply.github.com>
2019-09-12 15:51:22 -0700

    Update ApiKeys.cs

--- Models/ApiKeys.cs
+++ Models/ApiKeys.cs
@@ -1,1 +1,11 @@
 // Add your own Google API key Model
+using System;
+using System.Collections.Generic;
+
+namespace WeddingPlanner.Models
+{
+    public class ApiKeys
+    {
+        public string Google {get;set;} = "__ADD_YOUR_GOOGLE_API_KEY__";
+    }
+}
